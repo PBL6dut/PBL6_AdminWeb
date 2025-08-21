47db3edfb09ef5ec147cdae8dd2429af19aa29e0
Hoang Khoi <khoicitizens@gmail.com>
2025-08-21 19:22:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,9 @@
+import { Sidebar } from "../components/Sidebar"
+
+export const MainLayout = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
